88a2ad758fd6c6947ff7151869fb2d7256c908fb
Philipp Zettl <zettl.philipp@gmail.com>
2022-11-09 13:56:15 +0100

    drop unsafe literal evaluation

--- parser.py
+++ parser.py
@@ -1,9 +1,7 @@
 import sys
-import ast
 import json
 
 
 if __name__ == "__main__":
     for line in sys.stdin:
-        val = ast.literal_eval(line)
-        print(json.dumps(val))
+        print(json.dumps(json.loads(line)))
