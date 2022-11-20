ae3ca8ae9e98bfa69e6c083c7f5c57026373387a
Philipp Zettl <zettl.philipp@gmail.com>
2022-11-20 14:25:57 +0100

    build coverage again up to 100%

--- tests/parser/test_file_writer.py
+++ tests/parser/test_file_writer.py
@@ -2,10 +2,11 @@ import os
 import tempfile
 from unittest import TestCase, mock
 
+from parameterized import parameterized
 from pydantic import ValidationError
 
 from src.chain_smoker.file_loader import TestFileLoader
-from src.parser.file_writer import TestFileWriter
+from src.parser.file_writer import TestFileWriter, RewriteConfig
 
 
 class FileWriterTestCase(TestCase):
@@ -18,7 +19,7 @@ class FileWriterTestCase(TestCase):
                 'Protocol': 'HTTP/1', 'Headers': {},
             },
             'Response': {
-                'Status_code': 200, 'Body': {}, 'Headers': {},
+                'Status_code': 200, 'Body': {'foo': 1, 'bar': 'xxxxxx'}, 'Headers': {},
             }
         }
 
@@ -81,10 +82,20 @@ class FileWriterTestCase(TestCase):
             with self.assertRaises(ValidationError):
                 TestFileWriter(self.sample_request, None)
 
+            writer = TestFileWriter(self.sample_request, tmpfilepath)
+            writer.config.skip = {'/get/': ['GET']}
+
+            writer.write()
+            self.assertFalse(os.path.exists(tmpfilepath))
+
+            writer.config.skip = {}
+            writer.write()
+            self.assertTrue(os.path.exists(tmpfilepath))
+
     @mock.patch('src.chain_smoker.api_client.APIClient.get')
     def test_can_use_built_config(self, get_mock):
         get_mock.return_value = mock.Mock(
-            json=mock.Mock(return_value={}),
+            json=mock.Mock(return_value={'foo': 1, 'bar': 'xxxxxx'}),
             status_code=200
         )
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -98,3 +109,34 @@ class FileWriterTestCase(TestCase):
 
             # test runs successful
             self.assertIsNotNone(test_method.run())
+
+    @parameterized.expand([
+        ({'/get/': {'get': {'ignore_response': ['bar']}}}, {'foo': 1}),
+        ({'/get/': {'get': {'ignore_response': ['bar']}}}, {'foo': 1}),
+        ({'/get/': {'get': {'ignore_response': ['bar']}}}, {'foo': 1}),
+        ({'/get/': {'get': {}}}, {'foo': 1, 'bar': 'xxxxxx'}),
+        ({'/get/': {}}, {'foo': 1, 'bar': 'xxxxxx'}),
+        ({}, {'foo': 1, 'bar': 'xxxxxx'}),
+    ])
+    @mock.patch('src.chain_smoker.api_client.APIClient.get')
+    def test_clean_response(self, request_config, expected_response, get_mock):
+        get_mock.return_value = mock.Mock(
+            json=mock.Mock(return_value={'foo': 1, 'bar': 'xxxxxx'}),
+            status_code=200
+        )
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # you can e.g. create a file here:
+            temp_file_path = os.path.join(temp_dir, 'someFile.yaml')
+            writer = TestFileWriter(self.sample_request, temp_file_path)
+
+            writer.config.requests = request_config
+            config = writer._build_config()
+            test = config.get('tests')[0]
+
+            self.assertDictEqual(test.get('contains'), expected_response, test)
+
+
+class RewriteConfigTestCase(TestCase):
+    def test_from_file_unknown_file(self):
+        config = RewriteConfig.from_file('foo')
+        self.assertDictEqual(config.dict(), {'skip': {}, 'requests': {}, 'headers': {}})
