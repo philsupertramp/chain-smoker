3acb4cd07a5d51800b92569bc543c255b0a676cf
Philipp Zettl <zettl.philipp@gmail.com>
2023-04-08 12:01:03 +0200

    introduce color logger

--- src/chain_smoker/logger.py
+++ src/chain_smoker/logger.py
@@ -33,7 +33,6 @@ handlers = [file_handler, stdout_handler]
 
 logging.basicConfig(
     level=logging.INFO,
-    format='[%(asctime)s] %(levelname)s - %(message)s',
     handlers=handlers
 )
 logger = logging.getLogger('SMOKE_TESTER')
